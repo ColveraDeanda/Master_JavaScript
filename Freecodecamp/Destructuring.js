eddc8dea7049671979cb0c265878078baa060680
unknown <crisbmx2008@gmail.com>
2022-02-19 12:50:52 -0600

    wefwe

---
+++ Freecodecamp/Destructuring.js
@@ -0,0 +1,66 @@
+// Para asignar variables de un obj de la forma tradicional:
+const obj = {
+    nombre: 'Cristobal',
+    edad: 23
+}
+// let nombre = obj.nombre
+// let edad = obj.edad
+// console.log(nombre, edad)
+
+// Usando la sintaxis de desestructuración de ES6
+const { nombre, edad } = obj
+console.log(nombre, edad)
+
+// Para hacer lo mismo, pero asignandole algun nuevo nombre de variable seria:
+const { nombre: miNombre, edad: miEdad } = obj
+console.log(miNombre, miEdad)
+
+// NOTA: Se pueden desesctructurar 1 o mas propiedades, como se necesite.
+
+// Para desestructurar objetos dentro de otro objeto seria:
+let LOCAL_FORECAST = {
+    yesterday: { low: 61, high: 75 },
+    today: { low: 64, high: 77 },
+    tomorrow: { low: 68, high: 80 }
+};
+
+let { today: { low: lowToday, high: highToday } } = LOCAL_FORECAST
+console.log(lowToday, highToday);
+
+// Destructurando variables y arreglos:
+let a = 8 
+let b = 6;
+let [num1, num2] = [b, a];
+console.log(num1, num2)
+
+let otroArray = [1,2,3,4,5,6]
+const [i,j, ...k] = otroArray
+console.log(i,j,k)
+
+// Una forma de desestructurar un arreglo es usando la tecnica rest
+let numeros = [1,2,3,4,5,6,6,7]
+let [aa, bb , cc , ...newArray] = numeros
+console.log(aa, bb, cc, newArray)
+
+
+// Es posible pasar el objeto como parametro a una funcion y ahi mismo empezar la desectructuracion.
+const stats = {
+    max: 56.78,
+    standard_deviation: 4.34,
+    median: 34.54,
+    mode: 23.87,
+    min: -0.75,
+    average: 35.85
+  };
+  const half = ({max, min}) =>  {
+      return (max + min) / 2.0; 
+  }
+
+  console.log(half(stats))
+
+
+
+
+
+
+
--- Freecodecamp/ES6.js
+++ Freecodecamp/ES6.js
@@ -1,5 +1,5 @@
 // Es posible mutar (modificar) un arreglo a pesar de que sea const, esto se hace accediendo al elemento.
-const arr = [1,2,3,4,5]
+const arr = [1, 2, 3, 4, 5]
 arr[0] = 1000
 console.log(arr)
 
@@ -7,3 +7,26 @@ console.log(arr)
 Object.freeze(arr)
 arr[0] = 4444
 console.log(arr)
+
+
+// Uso de parametro por defecto. Solo se activara si no se especifica ese parametro al llamar la funcion.
+const greeting = (name = "Anonymous") => "Hello " + name;
+console.log(greeting("John"));
+console.log(greeting());
+
+// El parametro Rest: Sirve para crear variables con un numero variable de argumentos. Y se almacenan en un array.
+const sum = (...args) => {
+    return args.reduce((a, b) => a + b)
+}
+console.log(sum(1, 2, 3, 4))
+
+// Operador Spread: Permite expandir un arreglo donde se esperan múltiples 
+// parámetros o elementos. (Devuelve un arreglo desempacado).
+let array = [1,2,3,4,5]
+console.log(Math.max(...array))
+let array3 = []
+console.log(array3 = [...array])
+
+
+
+
