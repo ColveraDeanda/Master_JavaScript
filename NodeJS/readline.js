0276c9edf55e7ec8d69eec9c35a0b830d0d4dc63
unknown <crisbmx2008@gmail.com>
2022-03-16 10:55:26 -0600

    wefwef

---
+++ NodeJS/readline.js
@@ -0,0 +1,37 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+var personas = {
+    nombre: '',
+    comentarios: []
+}
+
+rl.question('Cuantos años tienes? ', (answer) => {
+    personas.nombre = answer;
+    rl.setPrompt('Dime un comentario: ') // Se setea el prompt.
+    rl.prompt() // Se imprime el prompt.
+
+});
+
+rl.on('line', (input) => { // El evento line se desparara cada vez que se presione enter.
+    if(input.trim() === 'salir') {
+        process.exit()
+    }
+    personas.comentarios.push(input)
+    printComments()
+    rl.setPrompt('Algun otro comentario? ')
+    rl.prompt()
+});
+
+function printComments() {
+    console.log('Tus comentarios son:');
+    let comments = [...personas.comentarios];
+    comments.map(value => {
+        console.log(value)
+    });
+}
+
