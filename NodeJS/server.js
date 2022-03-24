4990ed7ef7593c301a33446a8758ff03325dae4a
unknown <crisbmx2008@gmail.com>
2022-03-23 22:38:06 -0600

    wefwef

---
+++ NodeJS/server.js
@@ -0,0 +1,21 @@
+const http = require('http');
+
+http.createServer((req, res) => {
+    console.log(req.method, req.url);
+    res.writeHead(200, {'content-type': 'text/html'});
+    console.log(`${res.statusCode}`);
+
+    res.end(`
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body>
+        <h1>Hola mundo desde mi servidor NodeJS</h1>
+    </body>
+    </html>
+    `);
+}).listen(3000); // 3000 normalmente para servidores NodeJS
