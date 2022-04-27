13953dd4ad6a3133c851fb8fb44628a593d27c58
unknown <crisbmx2008@gmail.com>
2022-04-27 15:23:09 -0500

    wfwef

--- Curso_Angular_VictorRobles/controllers/project.controller.js
+++ Curso_Angular_VictorRobles/controllers/project.controller.js
@@ -1,10 +1,12 @@
+const fs = require('fs');
 const Project = require('../models/project');
+
 
 const projectController = {};
 
 projectController.saveProject = (req, res) => {
     const project = new Project();
-    const {name, description, category, langs, year} = req.body;
+    const { name, description, category, langs, year } = req.body;
 
     project.name = name;
     project.description = description;
@@ -14,9 +16,9 @@ projectController.saveProject = (req, res) => {
     project.image = null;
 
     project.save((err, project) => {
-        if(err) return res.status(500).send({message: err});
-        if(!project) return res.status(404).send({message: "No se ha podido guardar."});
-        return res.status(200).send({message: "Project saved...", project});
+        if (err) return res.status(500).send({ message: err });
+        if (!project) return res.status(404).send({ message: "No se ha podido guardar." });
+        return res.status(200).send({ message: "Project saved...", project });
     });
 }
 
@@ -24,17 +26,17 @@ projectController.saveProject = (req, res) => {
 projectController.getProject = (req, res) => {
     const id = req.params.id;
     Project.findById(id, (err, project) => {
-        if(err) return res.status(500).send({message: "Error al encontrar el projecto."});
-        if(!project) return res.status(404).send({message: "No se ha podido obtener el proyecto."});
-        return res.status(200).send({project});
+        if (err) return res.status(500).send({ message: "Error al encontrar el projecto." });
+        if (!project) return res.status(404).send({ message: "No se ha podido obtener el proyecto." });
+        return res.status(200).send({ project });
     });
 }
 
 projectController.getProjects = (req, res) => {
     Project.find({}).exec((err, projects) => {
-        if(err) return res.status(500).send({message: "Error al encontrar proyectos."});
-        if(!projects) return res.status(404).send({message: "No se ha podido obtener el proyecto."});
-        return res.status(200).send({projects});
+        if (err) return res.status(500).send({ message: "Error al encontrar proyectos." });
+        if (!projects) return res.status(404).send({ message: "No se ha podido obtener el proyecto." });
+        return res.status(200).send({ projects });
     });
 }
 
@@ -42,9 +44,9 @@ projectController.updateProject = (req, res) => {
     const id = req.params.id;
     const update = req.body;
 
-    Project.findByIdAndUpdate(id, update, {new: true}, (err, project) => {
-        if(err) return res.status(500).send({message: "Error al actualizar el projecto."});
-        if(!project) return res.status(404).send({message: "No se ha podido actualizar el proyecto."});
+    Project.findByIdAndUpdate(id, update, { new: true }, (err, project) => {
+        if (err) return res.status(500).send({ message: "Error al actualizar el projecto." });
+        if (!project) return res.status(404).send({ message: "No se ha podido actualizar el proyecto." });
         return res.status(200).send({
             message: "Project updated...",
             project: project
@@ -55,10 +57,34 @@ projectController.updateProject = (req, res) => {
 projectController.deleteProject = (req, res) => {
     const id = req.params.id;
     Project.findByIdAndRemove(id, (err, projectRemoved) => {
-        if(err) return res.status(500).send({message: "Error al eliminar el projecto."});
-        if(!projectRemoved) return res.status(404).send({message: "No se ha podido eliminar el proyecto."});
-        return res.status(200).send({message: "Proyecto eliminado..."});
+        if (err) return res.status(500).send({ message: "Error al eliminar el projecto." });
+        if (!projectRemoved) return res.status(404).send({ message: "No se ha podido eliminar el proyecto." });
+        return res.status(200).send({ message: "Proyecto eliminado..." });
     });
+}
+
+projectController.uploadImage = (req, res) => {
+    const id = req.params.id;
+    const fileName = req.files.image.path.split('\\')[1];
+    const imageType = req.files.image.type.split('/');
+
+    if (imageType[0] === 'image' && imageType[1] === 'jpeg' || imageType[0] === 'image' && imageType[1] === 'png') {
+        Project.findByIdAndUpdate(id, { image: fileName }, { new: true }, (err, projectUpdated) => {
+            if (err) return res.status(500).send({ message: "Error al actualizar al guardar la imagen." });
+            if (!projectUpdated) return res.status(404).send({ message: "No se ha podido guardar la imagen." })
+            res.status(200).send({
+                projectUpdated
+            });
+        });
+    } else {
+        fs.unlink(`./uploads/${fileName}`, (err) => {
+            if(err) throw err;
+        });
+        res.send({
+            message: "Porfavor sube una imagen con formato JPEG o PNG."
+        })
+    }
+
 }
 
 
--- Curso_Angular_VictorRobles/routes/project.route.js
+++ Curso_Angular_VictorRobles/routes/project.route.js
@@ -1,10 +1,14 @@
 const router = require('express').Router();
 const projectController = require('../controllers/project.controller');
 
+const multipart = require('connect-multiparty');
+const multipartMiddleware = multipart({ uploadDir: './uploads' });
+
 router.get('/', projectController.getProjects)
 router.get('/:id', projectController.getProject);
 router.post('/save-project', projectController.saveProject);
 router.put('/update-project/:id', projectController.updateProject);
 router.delete('/delete-project/:id', projectController.deleteProject);
+router.post('/upload-image/:id', multipartMiddleware, projectController.uploadImage);
 
 module.exports = router;
