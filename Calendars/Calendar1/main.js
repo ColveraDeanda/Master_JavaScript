1328e4e96aa55af2a84db41f1d270de5618e37d8
unknown <crisbmx2008@gmail.com>
2022-05-07 00:52:42 -0500

    wwfe

--- Calendars/Calendar1/main.js
+++ Calendars/Calendar1/main.js
@@ -1,115 +1,115 @@
-// let dataInfo = null
-// var firstTime = true;
-// var dataObj = {};
-// let calendar = document.querySelector('.calendar')
+let dataInfo = null
+var firstTime = true;
+var dataObj = {};
+let calendar = document.querySelector('.calendar')
 
-// const month_names = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+const month_names = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
 
-// let isLeapYear = (year) => {
-//     return (year % 4 === 0 && year % 100 !== 0 && year % 400 !== 0) || (year % 100 === 0 && year % 400 === 0)
-// }
+let isLeapYear = (year) => {
+    return (year % 4 === 0 && year % 100 !== 0 && year % 400 !== 0) || (year % 100 === 0 && year % 400 === 0)
+}
 
-// let getFebDays = (year) => {
-//     return isLeapYear(year) ? 29 : 28
-// }
+let getFebDays = (year) => {
+    return isLeapYear(year) ? 29 : 28
+}
 
-// let generateCalendar = (month, year) => {
+let generateCalendar = (month, year) => {
 
-//     let calendar_days = calendar.querySelector('.calendar-days')
-//     let calendar_header_year = calendar.querySelector('#year')
+    let calendar_days = calendar.querySelector('.calendar-days')
+    let calendar_header_year = calendar.querySelector('#year')
 
-//     let days_of_month = [31, getFebDays(year), 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    let days_of_month = [31, getFebDays(year), 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
-//     calendar_days.innerHTML = ''
+    calendar_days.innerHTML = ''
 
-//     let currDate = new Date()
-//     if (month == null) month = currDate.getMonth()
-//     if (!year) year = currDate.getFullYear()
+    let currDate = new Date()
+    if (month == null) month = currDate.getMonth()
+    if (!year) year = currDate.getFullYear()
 
-//     if (firstTime) {
-//         dataObj.mes = month
-//         // console.log(`Mes: ${month}`);    
-//         firstTime = false;
-//     }
+    if (firstTime) {
+        dataObj.mes = month
+        // console.log(`Mes: ${month}`);    
+        firstTime = false;
+    }
 
 
-//     let curr_month = `${month_names[month]}`
-//     month_picker.innerHTML = curr_month
-//     calendar_header_year.innerHTML = year
+    let curr_month = `${month_names[month]}`
+    month_picker.innerHTML = curr_month
+    calendar_header_year.innerHTML = year
 
-//     dataObj.year = year;
-//     // console.log('Año: ' + year);
+    dataObj.year = year;
+    // console.log('Año: ' + year);
 
-//     // get first day of month
+    //get first day of month
 
-//     let first_day = new Date(year, month, 1)
+    let first_day = new Date(year, month, 1)
 
-//     for (let i = 0; i <= days_of_month[month] + first_day.getDay() - 1; i++) {
-//         let day = document.createElement('div')
-//         if (i >= first_day.getDay()) {
-//             day.classList.add('calendar-day-hover')
-//             day.addEventListener('click', (value) => {
-//                 console.log('Same Day: ' + value.target.innerText);
-//                 dataObj.day = value.srcElement.innerText;
-//                 dataInfo = dataObj;
-//                 console.log('from main1.js ' + JSON.stringify(dataInfo));
-//             })
-//             day.innerHTML = i - first_day.getDay() + 1
-//             day.innerHTML += `<span></span>
-//                             <span></span>
-//                             <span></span>
-//                             <span></span>`
-//             if (i - first_day.getDay() + 1 === currDate.getDate() && year === currDate.getFullYear() && month === currDate.getMonth()) {
-//                 day.classList.add('curr-date')
-//             }
-//         }
-//         calendar_days.appendChild(day)
-//     }
-// }
+    for (let i = 0; i <= days_of_month[month] + first_day.getDay() - 1; i++) {
+        let day = document.createElement('div')
+        if (i >= first_day.getDay()) {
+            day.classList.add('calendar-day-hover')
+            day.addEventListener('click', (value) => {
+                console.log('Same Day: ' + value.target.innerText);
+                dataObj.day = value.srcElement.innerText;
+                dataInfo = dataObj;
+                console.log('from main1.js ' + JSON.stringify(dataInfo));
+            })
+            day.innerHTML = i - first_day.getDay() + 1
+            day.innerHTML += `<span></span>
+                            <span></span>
+                            <span></span>
+                            <span></span>`
+            if (i - first_day.getDay() + 1 === currDate.getDate() && year === currDate.getFullYear() && month === currDate.getMonth()) {
+                day.classList.add('curr-date')
+            }
+        }
+        calendar_days.appendChild(day)
+    }
+}
 
-// let month_list = calendar.querySelector('.month-list')
+let month_list = calendar.querySelector('.month-list')
 
-// month_names.forEach((e, index) => {
-//     let month = document.createElement('div')
-//     month.innerHTML = `<div data-month="${index}">${e}</div>`
-//     month.querySelector('div').onclick = () => {
-//         month_list.classList.remove('show')
-//         curr_month.value = index;
-//         dataObj.mes = curr_month.value
-//         // console.log(curr_month.value);
-//         generateCalendar(index, curr_year.value)
-//     }
-//     month_list.appendChild(month)
-// })
+month_names.forEach((e, index) => {
+    let month = document.createElement('div')
+    month.innerHTML = `<div data-month="${index}">${e}</div>`
+    month.querySelector('div').onclick = () => {
+        month_list.classList.remove('show')
+        curr_month.value = index;
+        dataObj.mes = curr_month.value
+        // console.log(curr_month.value);
+        generateCalendar(index, curr_year.value)
+    }
+    month_list.appendChild(month)
+})
 
-// let month_picker = calendar.querySelector('#month-picker')
+let month_picker = calendar.querySelector('#month-picker')
 
-// month_picker.onclick = () => {
-//     month_list.classList.add('show')
-// }
+month_picker.onclick = () => {
+    month_list.classList.add('show')
+}
 
-// let currDate = new Date()
+let currDate = new Date()
 
-// let curr_month = { value: currDate.getMonth() }
-// let curr_year = { value: currDate.getFullYear() }
+let curr_month = { value: currDate.getMonth() }
+let curr_year = { value: currDate.getFullYear() }
 
-// generateCalendar(curr_month.value, curr_year.value)
+generateCalendar(curr_month.value, curr_year.value)
 
-// document.querySelector('#prev-year').onclick = () => {
-//     --curr_year.value
-//     generateCalendar(curr_month.value, curr_year.value)
-// }
+document.querySelector('#prev-year').onclick = () => {
+    --curr_year.value
+    generateCalendar(curr_month.value, curr_year.value)
+}
 
-// document.querySelector('#next-year').onclick = () => {
-//     ++curr_year.value
-//     generateCalendar(curr_month.value, curr_year.value)
-// }
+document.querySelector('#next-year').onclick = () => {
+    ++curr_year.value
+    generateCalendar(curr_month.value, curr_year.value)
+}
 
-// let dark_mode_toggle = document.querySelector('.dark-mode-switch')
+let dark_mode_toggle = document.querySelector('.dark-mode-switch')
 
-// dark_mode_toggle.onclick = () => {
-//     document.querySelector('body').classList.toggle('light')
-//     document.querySelector('body').classList.toggle('dark')
-// }
+dark_mode_toggle.onclick = () => {
+    document.querySelector('body').classList.toggle('light')
+    document.querySelector('body').classList.toggle('dark')
+}
 
-// export { dataInfo }
+export { dataInfo }
